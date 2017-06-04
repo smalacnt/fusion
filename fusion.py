f9e240736506364450f9c2582c7c08f6e4bf238d
smalacnt <smalacnt@gmail.com>
2017-06-04 19:39:26 +0800

    add method interface & run.sh

--- fusion.py
+++ fusion.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,3 +11,16 @@ def hello(name=None):
     if name == None:
         name = 'world'
     return 'Hello, %s!' % name
+
+@app.route('/interface', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def interface():
+    app.logger.debug(request.get_json())
+    if request.method == 'POST':
+        json = request.get_json()
+        return json['name']
+    elif request.method == 'GET':
+        return 'GET'
+    elif request.method == 'PUT':
+        return 'PUT'
+    elif request.method == 'DELETE':
+        return 'DELETE'
